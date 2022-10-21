ccb8bd3d5320b5dfb04e0dc58a63ae3f0db7c0bd
RahulAid <raccess97@gmail.com>
2022-10-21 22:34:40 +0530

    Program to calculate Employee Part Time Wage

--- Program.cs
+++ Program.cs
@@ -4,13 +4,22 @@
     {
         static void Main(string[] args)
         {
-            int time = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
+
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int check = random.Next(0, 2);
-            if (check == time)
+            
+            //Computation
+            
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
 
@@ -18,7 +27,6 @@
             else
             {
                 empHrs = 0;
-
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
