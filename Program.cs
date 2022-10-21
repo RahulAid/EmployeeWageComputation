39d9f2cd561cc45b7b04dde8732d1dfea2f5a6b7
RahulAid <raccess97@gmail.com>
2022-10-21 22:55:51 +0530

    Program to calculate Employee Wage for a Month

--- Program.cs
+++ Program.cs
@@ -5,18 +5,20 @@
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NO_OF_WORKING_DAYS = 22;
         static void Main(string[] args)
         {
-            
             //Variables
 
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
+            int totalEmpWage = 0;
 
             //computation
 
+            Random random = new Random();
             int empCheck = random.Next(0, 3);
+
             switch (empCheck)
             {
                 case IS_PART_TIME:
@@ -27,11 +29,15 @@
                     break;
                 default:
                     empHrs = 0;
-                    break ;
+                    break;
             }
+
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+            totalEmpWage = empWage * NO_OF_WORKING_DAYS;
+            Console.WriteLine("Total working hours : " + empHrs * NO_OF_WORKING_DAYS );
+            
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+
         }
-        
     }
 }
