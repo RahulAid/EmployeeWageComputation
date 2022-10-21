8a2ba0c340193e9af062a1c4ebedf018f75ef5a8
RahulAid <raccess97@gmail.com>
2022-10-21 22:25:42 +0530

    Program to calculate Employee Wage

--- Program.cs
+++ Program.cs
@@ -4,18 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Employee Wage Computation");
-            int Fulltime = 1;
+            int time = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int check = random.Next(0, 2);
-            if (check == Fulltime)
+            if (check == time)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
+
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
         }
+        
     }
 }
